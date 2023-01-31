cb5d98a8f0f211a46387e237518a9bdfe5c2db1f
Mahipal <mahipal@cuttingchaitech.com>
2023-01-31 09:39:27 +0530

    project setup

---
+++ Swiftui_Continued_Learning/Swiftui_Continued_Learning/Swiftui_Continued_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_Continued_LearningApp.swift
+//  Swiftui_Continued_Learning
+//
+//  Created by Mahipal on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_Continued_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
